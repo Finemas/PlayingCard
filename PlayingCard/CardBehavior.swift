8407ee9065f49a79e811ae132617122dc87036b8
Jan Provazník <proovaznik@gmail.com>
2020-03-11 18:48:55 +0100

    The eighth Standford lecture
    Gesture (tap), Animations - UIView animations and DynamicBehavior

---
+++ PlayingCard/CardBehavior.swift
@@ -0,0 +1,59 @@
+//
+//  CardBehavior.swift
+//  PlayingCard
+//
+//  Created by Jan Provazník on 10/03/2020.
+//  Copyright © 2020 Jan Provazník. All rights reserved.
+//
+
+import UIKit
+
+class CardBehavior: UIDynamicBehavior {
+    lazy var collisionBehavior: UICollisionBehavior = {
+          let behavior = UICollisionBehavior()
+          behavior.translatesReferenceBoundsIntoBoundary = true
+          return behavior
+      }()
+      
+      lazy var itemBehavior: UIDynamicItemBehavior = {
+          let behavior = UIDynamicItemBehavior()
+          behavior.allowsRotation = false
+          behavior.elasticity = 1.0
+          behavior.resistance = 0
+          return behavior
+      }()
+    
+    private func push(_ item: UIDynamicItem) {
+        let push = UIPushBehavior(items: [item], mode: .instantaneous)
+        push.angle = (2*CGFloat.pi).arc4random
+        push.magnitude = CGFloat(1) + CGFloat(2).arc4random
+        push.action = { [unowned push, weak self] in
+            self?.removeChildBehavior(push)
+        }
+        addChildBehavior(push)
+    }
+    
+    func addItem(_ item: UIDynamicItem) {
+        collisionBehavior.addItem(item)
+        itemBehavior.addItem(item)
+        push(item)
+    }
+    
+    func removeItem(_ item: UIDynamicItem) {
+        collisionBehavior.removeItem(item)
+        itemBehavior.removeItem(item)
+    }
+    
+    override init() {
+        super.init()
+        
+        addChildBehavior(collisionBehavior)
+        addChildBehavior(itemBehavior)
+    }
+    
+    convenience init(in animator: UIDynamicAnimator) {
+        self.init()
+        
+        animator.addBehavior(self)
+    }
+}
